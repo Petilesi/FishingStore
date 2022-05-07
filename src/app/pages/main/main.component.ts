aad87c713c1089663b1e9c549e0f4442699bdf6b
Leskó Péter <h048216@stud.u-szeged.hu>
2022-05-07 18:25:57 +0200

    listings

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { product } from 'src/app/shared/models/product';
 import { ProductsService } from 'src/app/shared/services/products.service';
+import {Input } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -9,6 +10,9 @@ import { ProductsService } from 'src/app/shared/services/products.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+  @Input() loggedInUser?: firebase.default.User | null;
+
 
   productObject?: Observable<Array<product>>;
   constructor(private productService: ProductsService) {this.productObject = productService.getProducts() }
--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -4,5 +4,4 @@ export interface product{
    price: number;
    description: string;
    pictureSource: string; 
-   image_url: string;
 }
